0328f8b7e522ed94f5b685a73bd43742adb221ce
DenHax <whatxpw@gmail.com>
2024-04-23 23:41:58 +0300

    Рабочий grid. Input вынесен в отдельный класс вне grid. Изменены некоторые размеры, отступы, стилизация. Переносы выключены

--- src/App.tsx
+++ src/App.tsx
@@ -45,65 +45,43 @@ function App() {
     setAdvHidden(!isAdvHidden);
   }
 
-  // const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-  //   const { key } = event;
-  //   if (!inputRef.current || document.activeElement !== inputRef.current) {
-  //     if (key.match(/[0-9+\-*/(),=]|Backspace|Enter/)) {
-  //       event.preventDefault();
-  //       if (key === ("Enter" || "=")) {
-  //         calculateInput();
-  //       } else if (key === "Backspace") {
-  //         deleteOneChar();
-  //       } else {
-  //         setInput(input + key);
-  //       }
-  //     }
-  //   }
-  // };
-  //
-  // useEffect(() => {
-  //   document.addEventListener("keydown", handleKeyDown);
-  //   return () => {
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // });
-  //
   return (
     <>
+      <div className='inputfield'>
+        <input
+          className="input-field"
+          value={input}
+          readOnly
+          ref={inputRef}
+        />
+        <button id='advMode-btn' onClick={changeMode}>Adv</button>
+      </div>
       <div className='main'>
         <div className={`calc-grid ${isAdvHidden ? 'advNoHid' : 'advHid'}`}>
-          <input
-            className="input-field"
-            value={input}
-            readOnly
-            ref={inputRef}
-          />
           <button onClick={() => handleInput("%")}>%</button>
           <button onClick={() => handleInput("÷")}>÷</button>
           <button onClick={() => handleInput("×")}>×</button>
           <button onClick={() => handleInput("-")}>-</button>
           <button onClick={() => deleteOneChar()}>DEL</button>
-          <button onClick={() => handleInput("9")}>9</button>
-          <button onClick={() => handleInput("8")}>8</button>
           <button onClick={() => handleInput("7")}>7</button>
+          <button onClick={() => handleInput("8")}>8</button>
+          <button onClick={() => handleInput("9")}>9</button>
           <button id="plusBtn" onClick={() => handleInput("+")}>+</button>
           <button onClick={() => clearAllInput()}>AC</button>
-          <button onClick={() => handleInput("6")}>6</button>
-          <button onClick={() => handleInput("5")}>5</button>
           <button onClick={() => handleInput("4")}>4</button>
+          <button onClick={() => handleInput("5")}>5</button>
+          <button onClick={() => handleInput("6")}>6</button>
           <button onClick={() => handleInput("(")}>(</button>
-          <button onClick={() => handleInput("3")}>3</button>
-          <button onClick={() => handleInput("2")}>2</button>
           <button onClick={() => handleInput("1")}>1</button>
+          <button onClick={() => handleInput("2")}>2</button>
+          <button onClick={() => handleInput("3")}>3</button>
           <button id="equal" onClick={() => calculateInput()}>=</button>
           <button onClick={() => handleInput(")")}>)</button>
           <button id="zeroBtn" onClick={() => handleInput("0")}>0</button>
           <button onClick={() => handleInput(".")}>.</button>
           <button onClick={() => handleInput("+/-")}>+/-</button>
-          <button id='advMode-btn' onClick={changeMode}>Adv</button>
         </div >
-        <div className={`advFunc-${isAdvHidden ? 'flex' : 'hidden'}`}>
-          {/* <div className='advFuncs-flex' style={{ display: isAdvHidden ? 'none' : 'flex' }}> */}
+        <div className={`advFunc-${isAdvHidden ? 'grid' : 'hidden'}`}>
           <button onClick={() => handleInput("^")}>^</button>
           <button onClick={() => handleInput("√(")}>√</button>
           <button onClick={() => handleInput("sin(")}>sin</button>
@@ -111,7 +89,9 @@ function App() {
           <button onClick={() => handleInput("π")}>π</button>
           <button onClick={() => handleInput("e")}>e</button>
           <button onClick={() => handleInput("ln(")}>ln</button>
+          <button onClick={() => handleInput("abs(")}>|</button>
           <button onClick={() => handleInput("tg(")}>tg</button>
+          <button onClick={() => handleInput("ctg(")}>ctg</button>
         </div>
       </div>
     </>
